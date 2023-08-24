1d837d84c309f5474398757cf801ea8681bd1519
atomych <atomychtrue@gmail.com>
2023-08-24 21:38:25 +0300

    added default styles and start root

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
 <template>
   <router-view />
 </template>
+
+<style lang="scss">
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Open Sans", sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+}
+</style>
+
+<script>
+export default {
+  name: "App",
+
+  created() {
+    this.$router.push({ name: "game" });
+  },
+};
+</script>
