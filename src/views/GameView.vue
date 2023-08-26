87cd9bcb47b2ac28fae43fe31c2bb1d8f680a148
atomych <atomychtrue@gmail.com>
2023-08-26 14:33:01 +0300

    logic of display win-window

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,32 +1,49 @@
 <template>
-  <table class="board">
-    <ChessCell
-      v-for="item in game.field"
-      :key="item.id"
-      :cellColor="item.color"
-      :cellID="item.id"
-      :cellFigureColor="item.figure.color"
-      :cellFigureType="item.figure.type"
-      :cellSelected="
-        game.selectedCells.filter((cell) => cell == item.id).length > 0
-      "
-      @select="select"
-      @move="move"
-    />
-    <div class="bar left">
-      <div class="left__item item" v-for="item in 8" :key="item">
-        {{ game.side == 1 ? 9 - item : item }}
+  <div class="container">
+    <table class="board">
+      <ChessCell
+        v-for="item in game.field"
+        :key="item.id"
+        :cellColor="item.color"
+        :cellID="item.id"
+        :cellFigureColor="item.figure.color"
+        :cellFigureType="item.figure.type"
+        :cellSelected="
+          game.selectedCells.filter((cell) => cell == item.id).length > 0
+        "
+        @select="select"
+        @move="move"
+      />
+      <div class="bar left">
+        <div class="left__item item" v-for="item in 8" :key="item">
+          {{ game.side == 1 ? 9 - item : item }}
+        </div>
       </div>
-    </div>
-    <div class="bar bottom">
-      <div class="bottom__item item" v-for="item in 8" :key="item">
-        {{ game.side == 1 ? letters[item - 1] : letters[8 - item] }}
+      <div class="bar bottom">
+        <div class="bottom__item item" v-for="item in 8" :key="item">
+          {{ game.side == 1 ? letters[item - 1] : letters[8 - item] }}
+        </div>
       </div>
+    </table>
+    <div class="win-window" v-if="win !== false">
+      <div class="text">
+        {{ `Победили ${win == 1 ? "белые" : "черные"}!` }}
+      </div>
+      <button class="btn" @click="exit()">В главное меню</button>
     </div>
-  </table>
+  </div>
 </template>
 
 <style lang="scss">
+.container {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .board {
   width: 640px;
   height: 640px;
@@ -84,6 +101,38 @@
     bottom: -40px;
   }
 }
+
+.win-window {
+  position: absolute;
+
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #ffffffd4;
+
+  .text {
+    font-size: 25px;
+    font-weight: 700;
+    margin-bottom: 20px;
+  }
+
+  .btn {
+    font-family: "Open Sans", sans-serif;
+    font-weight: 400;
+    font-size: 20px;
+
+    border: none;
+    outline: none;
+    background: none;
+
+    cursor: pointer;
+  }
+}
 </style>
 
 <script>
@@ -113,21 +162,37 @@ export default {
   },
 
   methods: {
-    ...mapActions(["writeNewMove", "subscribeToGame"]),
+    ...mapActions([
+      "writeNewMove",
+      "subscribeToGame",
+      "exitFromGame",
+      "clearAll",
+    ]),
 
     select(position) {
       this.game.selectFigure(position);
     },
 
     move(position) {
-      this.game.moveFigure(position, (move) => {
-        this.writeNewMove(move);
-      });
+      this.game.moveFigure(
+        position,
+        (move) => {
+          this.writeNewMove(move);
+        },
+        (value) => {
+          if (value) this.exitFromGame();
+        }
+      );
+    },
+
+    exit() {
+      this.exitFromGame();
+      this.$router.push({ name: "home" });
     },
   },
 
   computed: {
-    ...mapGetters(["users", "userID", "moves"]),
+    ...mapGetters(["users", "userID", "moves", "win"]),
 
     letters() {
       return ["a", "b", "c", "d", "e", "f", "g", "h"];
@@ -139,6 +204,10 @@ export default {
       if (newValue.length != this.game.moves.length) {
         this.game.remoteMoveFigure(newValue[newValue.length - 1]);
       }
+    },
+
+    win(newValue) {
+      if (newValue != this.game.side) this.clearAll();
     },
   },
 };
